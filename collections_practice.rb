cbaae2abe6542d35b1a5241919883e9cbd816f80
rupinderkaur30-gif <rupi063094@gmail.com>
2021-02-10 00:11:40 -0500

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -6,7 +6,7 @@ end
  end
  def sort_array_char_count(array)
    array.sort do |x,y|
-      x <=> y
+      x.length <=> y.length
    end
  end
  def swap_elements(array)
@@ -16,3 +16,32 @@ end
   def reverse_array(array)
    array.reverse
   end
+  def kesha_maker(array)
+   array.each do |element|
+      element[2] = "$"
+   end
+end
+def find_a(array)
+   array.select do |word|
+      word.chr == "a"
+   end
+end
+def sum_array(array)
+   array.inject {|sum ,num | sum + num}
+end
+
+def add_s(array)
+   array.each_with_index.collect do |element, index| 
+   if index == 1
+      element
+   else 
+      element << "s"
+   end
+end
+end
+
+
+   
+
+
+
